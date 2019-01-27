7a4d7573812c5504f630d2efcfd2a7511d9c3d17
Patryk Domasik <patryk.domasik@outlook.com>
2019-01-27 23:48:42 +0100

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# The non-trading sundays
+
+Script genearating iCalendar (.ics) files with non-trading sundays in Poland for 2019 year.
+
+They are all-day events.
+
+You can choose one of six files without or with notifications at 12:00:
+
+1. [non_trade.ics](non_trade/non_trade.ics) - no notifications 
+2. [non_trade_one_day.ics](non_trade/non_trade_one_day.ics) - one day before
+3. [non_trade_two_days.ics](non_trade/non_trade_two_days.ics) - two days before
+4. [non_trade_three_days.ics](non_trade/non_trade_three_days.ics) - three days before
+5. [non_trade_four_days.ics](non_trade/non_trade_four_days.ics) - four days before
+6. [non_trade_five_days.ics](non_trade/non_trade_five_days.ics) - five days before
+
+Just download and import to Your calendar from file or import via the link.
+
+# Specification
+
+[RFC 5545](https://tools.ietf.org/html/rfc5545) via the [ics](https://www.npmjs.com/package/ics) package.
+
+# Donate
+
+BTC: bc1q8apw95kaejpathtvjzukeex4gdn87lznnv2yve
+
+ETH:  0xc1F0bF33a278c1d8AeC86192848152471711bD0a 
---
+++ app.js
@@ -0,0 +1,137 @@
+const { writeFileSync, existsSync, mkdirSync } = require('fs')
+const ics = require('ics')
+
+const year = 2019;
+/* Month in js are 0 based, so january is 0. */
+const months = 12;
+const nonTradeSundays = [];
+const events = [];
+const exceptionDates = [
+  new Date(2019, 3, 14), // niedziela przed Wielkanocą
+  new Date(2019, 11, 15), // I niedziela przed Bożym Narodzeniem
+  new Date(2019, 11, 22) // II niedziela przed Bożym Narodzeniem
+];
+const notificationHoursAndNames = [
+  [null, ''],
+  [12, '_one_day'],
+  [36, '_two_days'],
+  [60, '_three_days'],
+  [84, '_four_days'],
+  [108, '_five_days']
+];
+const fileDir = `${__dirname}\\non_trade`;
+
+/****** ENTRY HERE ******/
+checkAndSaveNonTradeSundaysAtSpecificYear(year);
+
+createDirIfNotExist();
+
+for (let i = 0; i < notificationHoursAndNames.length; i++) {
+
+  createEvents(notificationHoursAndNames[i][0]);
+  writeEventsToFile(notificationHoursAndNames[i][1]);
+
+  events.splice(0, events.length);
+
+}
+
+/*************************/
+
+function createDirIfNotExist(){
+  !existsSync(fileDir) && mkdirSync(fileDir);
+}
+
+function checkAndSaveNonTradeSundaysAtSpecificYear(year) {
+  const nonTradeSundaysAtYear = [];
+  for (let month = 0; month < months; month++) {
+    nonTradeSundaysAtYear.push(...getNonTradeSundaysAtSpecificMonth(year, month));
+  }
+  nonTradeSundays.push(...considerExcludedDates(nonTradeSundaysAtYear));
+}
+
+function getNonTradeSundaysAtSpecificMonth(year, month){
+  
+  let daysCount = daysInMonth(year, month);
+  let date = new Date(year, month);
+
+  const monthSundays = [];
+
+  for (let day = 1; day <= daysCount; day++) {
+    
+    date.setDate(day);
+
+    if (isSaturday(date)){
+      monthSundays.push(new Date(date));
+    }
+  }
+
+  return excludeLastSaturday(monthSundays);
+}
+// last day of last month
+function daysInMonth(year, month){
+  return new Date(year, month + 1, 0).getDate();
+}
+
+function isSaturday(date){
+  return date.getDay() == 0;
+}
+
+function excludeLastSaturday(monthSundays){
+  monthSundays.pop();
+  return monthSundays;
+}
+
+function considerExcludedDates(dates) {
+
+  const tmp = [];
+  for (let i = 0; i < dates.length; i++) {
+    const sunday = dates[i];
+  
+    let toExclude = false;
+  
+    for (let j = 0; j < exceptionDates.length; j++) {
+      const toExcludeSunday = exceptionDates[j];
+      
+      if(sunday.getTime() === toExcludeSunday.getTime()){
+        toExclude = true;
+        break;
+      }
+    }
+  
+    if (!toExclude) {
+      tmp.push(sunday);
+    }
+  }
+
+  return tmp;
+}
+
+function createEvents(hoursBeforeEventNotification) {
+  for (let i = 0; i < nonTradeSundays.length; i++) {
+
+    let sundayDate = [nonTradeSundays[i].getFullYear(), nonTradeSundays[i].getMonth() + 1, nonTradeSundays[i].getDate()];
+
+    const event = {
+      start: sundayDate,
+      end: sundayDate,
+      productId: 'Oscyp//https://github.com/oscyp',
+      title: 'Niedziela niehandlowa',
+      alarms: hoursBeforeEventNotification !== null
+              ? [{ action: 'display', trigger: { hours: hoursBeforeEventNotification, before: true } }]
+              : ''
+    }
+
+    events.push(event);
+  }
+}
+
+function writeEventsToFile(fileNotificationName){
+  ics.createEvents(events, (error, value) => {
+    if (error) {
+      console.log(error)
+      return
+    }
+    writeFileSync(`${fileDir}\\non_trade${fileNotificationName}.ics`, value, { recursive: true })
+  })
+}
+
